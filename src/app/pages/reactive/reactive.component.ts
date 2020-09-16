e5791a80124e8f692a836c76a29b2576bc0707ae
Andres Velasquez <monkey.velasquez.1982@gmail.com>
2020-09-16 14:29:11 -0500

    form reactivos 16 sep 2020

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -15,11 +15,21 @@ export class ReactiveComponent implements OnInit {
 
   ngOnInit(): void {}
 
+  get nombreNoValido() {
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  }
+
   crearFormulario() {
     this.forma = this.fb.group({
-      nombre: [''],
-      apellido: [''],
-      correo: [''],
+      nombre: ['', [Validators.required, Validators.minLength(5)]],
+      apellido: ['', Validators.required],
+      correo: [
+        '',
+        [
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
+          Validators.required,
+        ],
+      ],
     });
   }
 
